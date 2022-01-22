6967e4b017881e956010f6c735d4d870be9d9e43
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2022-01-22 22:29:23 +0700

    initial project

---
+++ ChitChat/ChitChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChitChatApp.swift
+//  ChitChat
+//
+//  Created by Siroratt Suntronsuk on 22/1/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct ChitChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
