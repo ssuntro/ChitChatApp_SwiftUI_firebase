41aeb0d2b16fdb5e8fe2a78db776c495691c37ac
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2022-01-29 22:43:38 +0700

    [AC-x] update field in RecentMessage

--- ChitChat/MainChatView.swift
+++ ChitChat/MainChatView.swift
@@ -51,14 +51,11 @@ struct RecentMessage: Identifiable {
     
     let image = "https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=67773a9d419786091c958b2ad08eae5e"
     let email: String
-    let text: String
+    let lastestLog: String
     let timestamp = "6 Days ago"
 }
 
-let recentMessages = [RecentMessage(email: "one@a.com", text: "one ja"),
-                      RecentMessage(email: "two@a.com", text: "two ja"),
-                      RecentMessage(email: "one@a.com", text: "one ja"),
-                      RecentMessage(email: "two@a.com", text: "two ja")]
+
 
 struct MainChatView_Previews: PreviewProvider {
     static var previews: some View {
